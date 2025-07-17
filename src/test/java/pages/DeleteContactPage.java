ef9d7f92d23293ad3cf89f5dcc4e29012f13ba61
Srividhyaa Vasudevan <vasrira141@gmail.com>
2025-07-18 02:55:32 +0530

    latest_changes

--- src/test/java/pages/DeleteContactPage.java
+++ src/test/java/pages/DeleteContactPage.java
@@ -1,5 +1,45 @@
 package pages;
 
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+
 public class DeleteContactPage {
 
+    WebDriver driver;
+    WebDriverWait wait;
+
+    public DeleteContactPage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    // Delete a contact by name
+    public void deleteContactByName(String contactName) {
+        WebElement deleteBtn = wait.until(ExpectedConditions.elementToBeClickable(
+            By.xpath("//div[contains(text(),'" + contactName + "')]/following-sibling::button[contains(text(),'Delete')]")));
+        deleteBtn.click();
+    }
+
+    // Override browser confirmation alert
+    public void overrideConfirmationAlert() {
+        ((JavascriptExecutor) driver).executeScript("window.confirm = function(){return true;}");
+    }
+
+    // Verify contact is deleted
+    public boolean isContactDeleted(String contactName) {
+        try {
+            wait.until(ExpectedConditions.invisibilityOfElementLocated(
+                By.xpath("//div[contains(text(),'" + contactName + "')]")));
+            return true;
+        } catch (TimeoutException e) {
+            return false;
+        }
+    }
+
+    // Refresh the page
+    public void refreshPage() {
+        driver.navigate().refresh();
+    }
 }
--- src/test/java/pages/InputValidationPage.java
+++ src/test/java/pages/InputValidationPage.java
@@ -1,5 +1,46 @@
 package pages;
 
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class InputValidationPage {
 
+    WebDriver driver;
+    WebDriverWait wait;
+
+    public InputValidationPage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    public void loginAndOpenAddContactForm() {
+        driver.get("https://thinking-tester-contact-list.herokuapp.com/");
+        driver.findElement(By.id("email")).sendKeys("vasrira@gmail.com");
+        driver.findElement(By.id("password")).sendKeys("Srisathiya@15");
+        driver.findElement(By.id("submit")).click();
+        driver.findElement(By.id("add-contact")).click();
+    }
+
+    public void enterLongFirstName() {
+        String longName = "A".repeat(301);
+        WebElement firstNameField = driver.findElement(By.id("firstName"));
+        firstNameField.sendKeys(longName);
+    }
+
+    public int getFirstNameLength() {
+        WebElement firstNameField = driver.findElement(By.id("firstName"));
+        return firstNameField.getAttribute("value").length();
+    }
+
+    public void enterUnicodeInAddress() {
+        WebElement street1 = driver.findElement(By.id("street1"));
+        String unicodeText = "\ud83c\udfe0 住所 日本語文字";
+        street1.sendKeys(unicodeText);
+    }
+
+    public String getAddressFieldValue() {
+        return driver.findElement(By.id("street1")).getAttribute("value");
+    }
 }
